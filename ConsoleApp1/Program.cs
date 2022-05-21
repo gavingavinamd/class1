e5aaea442b0145bd8f8c34483361655cafef2606
GavinChou <gavingavinamd@gmail.com>
2022-05-21 11:27:39 +0800

    美國隊長沒有拉取

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World2!");
+
+Console.WriteLine("美國隊長沒有拉取");
+Console.WriteLine("美國隊長有要PUSH");
